e2419e99fd7b5d25756be184c6f1a05116eed706
CamiloDavid <camilodavid9910@gmail.com>
2024-08-09 07:12:38 -0500

    se agrego import municipios

--- src/MapView.js
+++ src/MapView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-
+import {municipios} from './diccionario';
 
 // Corrige los íconos rotos
 delete L.Icon.Default.prototype._getIconUrl;
@@ -20,11 +20,18 @@ const MapView = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={[2.45, -76.61]}>
-                <Popup>
-                    ¡Aquí está el departamento del Cauca!
-                </Popup>
-            </Marker>
+            {municipios.map((municipio, index) => (
+                <Marker key={index} position={municipio.posicion}>
+                    <Popup>
+                        <strong>{municipio.nombre}</strong><br />
+                        {municipio.reseña}<br />
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <img src={municipio.imagen} alt={municipio.nombre} style={{ width: '48%', height: 'auto' }} />
+                            <img src={municipio.imagen1} alt={municipio.nombre} style={{ width: '48%', height: 'auto' }} />
+                        </div>
+                    </Popup>
+                </Marker>
+            ))}
         </MapContainer>
     );
 }
