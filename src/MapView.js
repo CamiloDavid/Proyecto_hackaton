8d8b067fc71b7b23a281bd2fc9ce28777d0308e0
CamiloDavid <camilodavid9910@gmail.com>
2024-08-09 06:38:57 -0500

    Primer Piloto del Proyecto

---
+++ src/MapView.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Corrige los íconos rotos
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+const MapView = () => {
+    return (
+        <MapContainer center={[2.45, -76.61]} zoom={10} style={{ height: '100vh', width: '100%' }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={[2.45, -76.61]}>
+                <Popup>
+                    ¡Aquí está el departamento del Cauca!
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
+
+export default MapView;
---
+++ src/diccionario.js
@@ -0,0 +1,45 @@
+const municipios = [
+    {
+        nombre: "El tambo",
+        posicion: [2.4525, -76.7269],
+        reseña: "El Tambo es un municipio colombiano ubicado en el departamento del Cauca, perteneciente al área metropolitana de Popayán, capital departamental de la que dista aproximadamente 33 km. Cuenta con una población aproximada de 53.769 habitantes. Es un municipio rico en recursos naturales, de sus montañas nace el agua que abastece a miles de personas.",
+        imagen: "https://es.m.wikipedia.org/wiki/Archivo:Blue-gray_Tanager_(45494712395).jpg",
+        imagen1: "https://es.m.wikipedia.org/wiki/Archivo:Heliconia_rostrata1.jpg"
+    },
+    {
+        nombre: "Popayán",
+        posicion: [2.441, -76.606],
+        reseña: "Popayán, conocida como la Ciudad Blanca por el color de sus edificios coloniales, es la capital del departamento del Cauca en Colombia. Es famosa por su arquitectura bien conservada, iglesias históricas y una rica tradición cultural, especialmente durante la Semana Santa, cuando se celebran procesiones religiosas reconocidas internacionalmente. Además, Popayán es un importante centro gastronómico, conocido por sus delicias tradicionales como la empanada de pipián.",
+        imagen: "https://es.m.wikipedia.org/wiki/Archivo:PigeonMonceau_(cropped).jpg",
+        imagen1: "https://es.m.wikipedia.org/wiki/Archivo:Flower_of_Spathodea_campanulata_at_Muzhappilangad_.jpg"
+    },
+    {
+        nombre: "Puracé",
+        posicion: [2.5111, -76.2611],
+        reseña: "Puracé es un municipio ubicado en el departamento del Cauca, Colombia, conocido por su impresionante paisaje montañoso y su biodiversidad. El principal atractivo de la zona es el Parque Nacional Natural Puracé, que alberga el volcán Puracé, uno de los más activos del país. Este parque es un paraíso para los amantes de la naturaleza, ofreciendo senderos que atraviesan páramos, bosques y fuentes termales. Puracé también es hogar de comunidades indígenas que conservan sus tradiciones ancestrales, lo que añade una dimensión cultural única a la visita. Es un destino ideal para el ecoturismo y la aventura.",
+        imagen: "https://es.m.wikipedia.org/wiki/Archivo:AndeanCondorMale.jpg",
+        imagen1: "https://es.m.wikipedia.org/wiki/Archivo:Papaver_somniferum_-_K%C3%B6hler%E2%80%93s_Medizinal-Pflanzen-102.jpg"
+    },
+    {
+        nombre: "Guapi",
+        posicion: [2.5711, -77.8867],
+        reseña: "Guapi municipio ubicado en la costa pacífica del departamento del Cauca, Colombia, conocido por su rica biodiversidad y su cultura afrocolombiana. Este lugar se caracteriza por su alta humedad, coberturas boscosas significativas, y temperaturas cálidas, creando un entorno óptimo para el desarrollo de una diversa fauna y flora tanto terrestre como acuática.",
+        imagen: "https://inaturalist-open-data.s3.amazonaws.com/photos/11429115/medium.jpg",
+        imagen1: "https://inaturalist-open-data.s3.amazonaws.com/photos/1975915/medium.JPG"
+    },
+    {
+        nombre: "Santa Rosa",
+        posicion: [2.5111, -76.2611],
+        reseña: "Santa Rosa ubicada en la Bota Caucana, cuenta con más de 1.033 especies de aves, de las cerca de 1.400 que hay en el Cauca, se encuentran diferentes pisos términos, ecosistemas de paramo, ecosistemas de bosque altoandino, ecosistemas andinos, lo que hace que estos diferentes tipos de hábitat sean aptos para especies endémicas, especies migratorias boreales y migratorias australes.",
+        imagen: "https://imagenes.eltiempo.com/files/image_652_366/uploads/2022/06/02/629966ba3af30.jpeg",
+        imagen1: "https://minciencias.gov.co/sites/default/files/ckeditor_files/Bota%20Caucana.jpg"
+    },
+    {
+        nombre: "Inza",
+        posicion: [2.5522, -76.0664],
+        reseña: "Según la CRC, durante los últimos años se ha llevado a cabo registros de más de 20 especies de fauna silvestre, mediante la instalación de cámaras fauna, en la vereda Río Sucio, del municipio de Inzá. Entre las especies registradas están Pumas concolor, venados de páramo, dantas de montaña, tigrillos y zarigüeyas..",
+        imagen: "https://www.periodicolacampana.com/wp-content/uploads/2023/07/Foto-inicio-420x470.jpeg",
+        imagen1: "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/201207041587544064.jpeg?x-oss-process=image/format,webp/resize,s_422&v=1.3"
+    },
+    // Agrega más municipios según sea necesario
+];
